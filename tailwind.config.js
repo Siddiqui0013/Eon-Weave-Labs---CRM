eb833a39e586402cb1234e9a17e8810ef31709cd
hasanbut17314 <hasanbut17314@gmail.com>
2025-01-15 16:38:38 +0500

    feat: add SaleView component and update BdoRoutes to include sales report view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-  	extend: {
-  		colors: {
-  			primary: '#E75B10',
-  			secondary: '#6379F4',
-  			dark: '#0F1E25',
-  			card: '#3F3F3F'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	], theme: {
+		extend: {
+			colors: {
+				primary: '#E75B10',
+				secondary: '#6379F4',
+				dark: '#0F1E25',
+				card: '#3F3F3F'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 }
