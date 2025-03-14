e6d4621befd479f51bd974d7cc87aaacd3c8da9e
hasanbut17314 <hasanbut17314@gmail.com>
2025-03-14 23:59:46 +0500

    added prod baseUrls

--- src/lib/SocketManager.ts
+++ src/lib/SocketManager.ts
@@ -9,8 +9,8 @@ interface SocketManagerProps {
     isAuth: boolean;
 }
 
-//const base_url = 'https://ewlcrm-backend.vercel.app';
-const base_url = 'http://localhost:8000';
+const base_url = 'https://ewlcrm-backend.vercel.app';
+//const base_url = 'http://localhost:8000';
 
 const SocketManager: React.FC<SocketManagerProps> = ({ isAuthenticated, isAuth }) => {
     const dispatch = useDispatch<AppDispatch>();
